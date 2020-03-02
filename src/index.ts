c6dbcce2fa0f129592232920559910ec0e06b82d
Alvin Huynh <ahuynh5190@gmail.com>
2020-03-02 08:37:39 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -131,7 +131,9 @@ class ConsolidatedLogger {
 
     setupInputChangeLogEvent(inputChangeMetaData: InputChangeMetaData) {
         const input = document.querySelector(inputChangeMetaData.selector);
-
+        
+        // TODO: Consider throttling this since it can lead to spamming the backend.
+        // Potentially can batch this into one call instead of once per value change.
         input.addEventListener('input', () => {
             this._sendLogEvent({
                 ...inputChangeMetaData,
